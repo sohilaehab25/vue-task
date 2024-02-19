8f29ec9234ab0693ec5270ddcf317839624b5f0a
sohilaehab25 <114905257+sohilaehab25@users.noreply.github.com>
2024-02-19 02:55:42 +0200

    labvue
    task with html css3 bootstrap and vue

---
+++ arrayofbook.js
@@ -0,0 +1,201 @@
+ books=[
+    {
+        Bookname:"Book1",
+        isbn:12345,
+        Category:"categorytwo",
+        Auther:"authertwo",
+        price:1000, 
+        pages:20,
+        image:"./1.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book2",
+        isbn:23586,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:2000, 
+        pages:2000,
+        image:"images/2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book3",
+        isbn:78954,
+        Category:"categorytwo",
+        Auther:"autherone",
+        price:3000, 
+        pages:3000,
+        image:"images/3.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book4",
+        isbn:852134,
+        Category:"categoryone",
+        Auther:"authertwo",
+        price:5000, 
+        pages:5000,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book5",
+        isbn:23545,
+        Category:"categortwo",
+        Auther:"autherone",
+        price:100, 
+        pages:1000,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book6",
+        isbn:23568,
+        Category:"categoryone",
+        Auther:"authertwo",
+        price:200, 
+        pages:200,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book7",
+        isbn:236598,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:4500, 
+        pages:8900,
+        image:"images/2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book8",
+        isbn:12345,
+        Category:"categorytwo",
+        Auther:"authertwo",
+        price:8540, 
+        pages:1250,
+        image:"1.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book9",
+        isbn:2356,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:7800, 
+        pages:0,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book10",
+        isbn:12345,
+        Category:"categorytwo",
+        Auther:"authertwo",
+        price:1000, 
+        pages:1000,
+        image:"./1.jpg",
+        wishlist:"addtowishlist"
+
+
+    },
+    {
+        Bookname:"Book11",
+        isbn:23586,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:2000, 
+        pages:2000,
+        image:"images/2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book12",
+        isbn:78954,
+        Category:"categorytwo",
+        Auther:"autherone",
+        price:3000, 
+        pages:3000,
+        image:"images/3.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book13",
+        isbn:852134,
+        Category:"categoryone",
+        Auther:"authertwo",
+        price:5000, 
+        pages:5000,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book14",
+        isbn:23545,
+        Category:"categortwo",
+        Auther:"autherone",
+        price:100, 
+        pages:30,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book15",
+        isbn:23568,
+        Category:"categoryone",
+        Auther:"authertwo",
+        price:200, 
+        pages:200,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book16",
+        isbn:236598,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:4500, 
+        pages:8900,
+        image:"images/2.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book17",
+        isbn:12345,
+        Category:"categorytwo",
+        Auther:"authertwo",
+        price:8540, 
+        pages:1250,
+        image:"1.jpg",
+        wishlist:"addtowishlist"
+
+    },
+    {
+        Bookname:"Book18",
+        isbn:2356,
+        Category:"categoryone",
+        Auther:"autherone",
+        price:7800, 
+        pages:4500,
+        image:"./2.jpg",
+        wishlist:"addtowishlist"
+
+    }
+]
